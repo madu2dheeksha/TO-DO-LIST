f6ec71b2f5d7ce78f252e963514c7012022c6827
MADU DHEEKSHA <152462484+madu2dheeksha@users.noreply.github.com>
2024-06-16 23:40:35 +0530

    Add files via upload
    By using HTML,CSS AND JAVASCRIPT, i had prepared a to-do list , it took me 2-3 hours .

---
+++ HTML CODE (to-do list).py
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dheeksha's To-Do List </title>
+    <link rel="stylesheet" href="stylee.css">
+</head>
+<body>
+    <div class="container">
+        <div class="todo-app">
+            <h2>dheeksha's To-Do List <img src="images.png"></h2>
+
+            <div class="row">
+                <input type="text" name="" id="input-box" placeholder="Add your Text">
+                <button onclick="add task"()">ADD TASK</button>
+            </div>
+
+            <ul id="list-container">
+                <!-- <li class="checked">Task 1</li>
+                <li>Task 2</li>
+                <li>Task 3</li> -->
+            </ul>
+        </div>
+    </div>
+    <script src="TODOLIST.JS"></script>
+</body>
+</html>
